90571f93ba33f63628be0d6614f76daeea13c94b
OlegGlo <olegglotov99@gmail.com>
2025-03-07 02:15:13 -0300

    edit

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"seFKe8rbjehiVXdOMAOUQvg783DmsDvl4BsAC7lvWrY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
