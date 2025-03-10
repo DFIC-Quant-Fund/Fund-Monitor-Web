2434a63db94808708b9b9471ab9948d8cfbdb8a5
OlegGlo <olegglotov99@gmail.com>
2025-03-10 01:02:14 -0300

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
-      <h1 className="text-[#800000] text-6xl font-bold">DFIC</h1>
-    </div>
-  );
+
+    const performanceData = [
+        {
+            date: "2024-01-01",
+            inception_return: "10%",
+        },
+
+    ];
+
+    return (
+        <div className="min-h-screen bg-white flex items-center justify-center">
+            <h1 className="text-[#800000] text-6xl font-bold">DFIC</h1>
+        </div>
+    );
 }
