a536671d614704ae0b33bcfc39e7e714c33d03bd
Cameron Beneteau <cameron.beneteau@gmail.com>
2025-03-21 01:22:13 -0400

    Update general API logic

--- src/utils/apiBase.tsx
+++ src/utils/apiBase.tsx
@@ -1,1 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+// DO NOT COMMIT THIS FILE WITH THE API BASE URL SET TO A LOCAL SERVER
+// Change this to the desired API base URL when developing locally (i.e. http://127.0.0.1:5555)
+
+export const API_BASE_URL = 'https://api.degrootefinance.com';
