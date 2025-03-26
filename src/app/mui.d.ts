e611626a15ce96b6f397dc179e42556066c34bda
Tvesha Shah <tveshashah13@gmail.com>
2025-03-25 22:03:16 -0400

    deleting an unused file

--- src/app/mui.d.ts
+++
@@ -1,8 +0,0 @@
-import '@mui/material/Paper';
-
-// declaration file for types used in MUI 
-declare module '@mui/material/Paper' {
-  interface PaperPropsVariantOverrides {
-    summary: true;
-  }
-}
