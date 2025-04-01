9883039ed112f32b6812e1a9745f58bca3d3797d
Ryan Calicchia <144076558+calicchr@users.noreply.github.com>
2025-04-01 00:10:07 -0400

    force header bar to stay (#46)

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export default function Header() {
     const router = useRouter();
     return (
-        <nav className="bg-[#800000] text-white shadow-lg">
+        <nav className="bg-[#800000] text-white shadow-lg sticky top-0 z-50">
             <div className="mx-auto px-2 sm:px-18">
                 <div className="flex justify-between items-center h-22 sm:h-22 py-2 sm:py-0">
                     {/* Logo */}
@@ -24,30 +24,30 @@ export default function Header() {
                         <Link
                             href="/holdings"
                             className="
-                px-2 py-1 rounded-md text-xs font-bold 
-                hover:bg-[#600000] transition-colors duration-200
-                sm:px-3 sm:py-2 sm:text-base
-              "
+                                px-2 py-1 rounded-md text-xs font-bold 
+                                hover:bg-[#600000] transition-colors duration-200
+                                sm:px-3 sm:py-2 sm:text-base
+                            "
                         >
                             Holdings
                         </Link>
                         <Link
                             href="/performance"
                             className="
-                px-2 py-1 rounded-md text-xs font-bold 
-                hover:bg-[#600000] transition-colors duration-200
-                sm:px-3 sm:py-2 sm:text-base
-              "
+                                px-2 py-1 rounded-md text-xs font-bold 
+                                hover:bg-[#600000] transition-colors duration-200
+                                sm:px-3 sm:py-2 sm:text-base
+                            "
                         >
                             Performance
                         </Link>
                         <Link
                             href="/transactions"
                             className="
-                px-2 py-1 rounded-md text-xs font-bold 
-                hover:bg-[#600000] transition-colors duration-200
-                sm:px-3 sm:py-2 sm:text-base
-              "
+                                px-2 py-1 rounded-md text-xs font-bold 
+                                hover:bg-[#600000] transition-colors duration-200
+                                sm:px-3 sm:py-2 sm:text-base
+                            "
                         >
                             Transactions
                         </Link>
@@ -60,10 +60,10 @@ export default function Header() {
                             router.push("/login");
                         }}
                         className="
-                px-2 py-1 rounded-md text-xs font-bold 
-                hover:bg-[#600000] transition-colors duration-200
-                sm:px-3 sm:py-2 sm:text-base
-              "
+                            px-2 py-1 rounded-md text-xs font-bold 
+                            hover:bg-[#600000] transition-colors duration-200
+                            sm:px-3 sm:py-2 sm:text-base
+                        "
                     >
                         Logout
                     </Link>
